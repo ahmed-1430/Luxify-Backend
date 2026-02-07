a996b8208c345683b3fbf36035a056aa233683cc
ahmed-1430 <ahmedtexting@gmail.com>
2026-02-08 04:41:10 +0600

    cart schema model added

---
+++ models/Cart.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = new mongoose.Schema({
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    name: String,
+    price: Number,
+    image: String,
+    quantity: { type: Number, required: true, min: 1 },
+});
+
+const cartSchema = new mongoose.Schema(
+    {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+        items: [cartItemSchema],
+        subtotal: { type: Number, default: 0 },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
